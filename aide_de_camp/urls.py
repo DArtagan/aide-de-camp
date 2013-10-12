e0256258dd4fe9e110336f7d72d44c7dc01dd47e
William Weiskopf <worldwidewilly@comcast.net>
2013-10-12 12:03:47 -0600

    Initial commit after initializing aide-de-camp with Django.

---
+++ aide_de_camp/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'aide_de_camp.views.home', name='home'),
+    # url(r'^aide_de_camp/', include('aide_de_camp.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
