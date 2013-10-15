845fe3facdcab22c64d113561c2f3a06cc2ff5a0
William Weiskopf <worldwidewilly@comcast.net>
2013-10-14 19:32:49 -0600

    Initial class definitions for companies: Company, Contact, Position

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    notes = models.TextField()
+
+class Contact(models.Model):
+    company = models.ManyToManyField(Company)
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    notes = models.TextField()
+
+class Position(models.Model):
+    company = models.ForeignKey(Company)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
+    start_date = models.DateField()
+    date_noted = models.DateField(auto_now_add=True)
+    notes = models.TextField()
