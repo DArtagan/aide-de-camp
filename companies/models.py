6410f660e67a7eea290242e5a31608e85cd3e149
William Weiskopf <worldwidewilly@comcast.net>
2013-10-29 13:47:28 -0600

    Turns out dateFields need to be blank and null to not be required.

--- companies/models.py
+++ companies/models.py
@@ -30,10 +30,10 @@ class Position(models.Model):
     company = models.ForeignKey(Company)
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=50, blank=True)
-    start_date = models.DateField(null=True)
+    start_date = models.DateField(blank=True, null=True)
     date_noted = models.DateField(auto_now_add=True)
     app_portal = models.CharField(max_length=500, blank=True)
-    apply_deadline = models.DateField(null=True)
+    apply_deadline = models.DateField(blank=True, null=True)
     notes = models.TextField(blank=True)
 
     def get_absolute_url(self):
