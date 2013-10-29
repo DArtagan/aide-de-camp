5646364cecf14aefe65be2eea0f466a943667e69
William Weiskopf <worldwidewilly@comcast.net>
2013-10-29 13:34:32 -0600

    Allowing for blank fields, added application related fields to Position.

--- companies/models.py
+++ companies/models.py
@@ -3,8 +3,8 @@ from django.core.urlresolvers import reverse
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
-    notes = models.TextField()
+    location = models.CharField(max_length=50, blank=True)
+    notes = models.TextField(blank=True)
 
     def get_absolute_url(self):
         return reverse('company:company_detail', args=[self.pk])
@@ -13,12 +13,12 @@ class Company(models.Model):
         return self.name
 
 class Contact(models.Model):
-    company = models.ManyToManyField(Company)
+    company = models.ManyToManyField(Company, null=True)
     name = models.CharField(max_length=50)
-    position = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    notes = models.TextField()
+    position = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    notes = models.TextField(blank=True)
 
     def get_absolute_url(self):
         return reverse('Contact:contact_detail', args=[self.pk])
@@ -29,10 +29,12 @@ class Contact(models.Model):
 class Position(models.Model):
     company = models.ForeignKey(Company)
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=50)
-    start_date = models.DateField()
+    location = models.CharField(max_length=50, blank=True)
+    start_date = models.DateField(null=True)
     date_noted = models.DateField(auto_now_add=True)
-    notes = models.TextField()
+    app_portal = models.CharField(max_length=500, blank=True)
+    apply_deadline = models.DateField(null=True)
+    notes = models.TextField(blank=True)
 
     def get_absolute_url(self):
         return reverse('Position:position_detail', args=[self.pk])
