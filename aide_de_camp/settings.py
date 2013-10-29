1f943b5f5c4bbf2f628cd1212f74ab0bb78c2540
William Weiskopf <worldwidewilly@comcast.net>
2013-10-29 00:26:11 -0600

    Added a homepage.

--- aide_de_camp/settings.py
+++ aide_de_camp/settings.py
@@ -1,4 +1,5 @@
 # Django settings for aide_de_camp project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -8,6 +9,8 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
@@ -111,6 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + '/templates/'
 )
 
 INSTALLED_APPS = (
