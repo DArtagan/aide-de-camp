51873d5a8d4feea95ba6127e2625e4d86ab8ffce
William Weiskopf <worldwidewilly@comcast.net>
2013-11-03 22:21:45 -0700

    Reset South.

---
+++ companies/migrations/0001_initial.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Company'
+        db.create_table(u'companies_company', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('location', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('notes', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], unique=True)),
+        ))
+        db.send_create_signal(u'companies', ['Company'])
+
+        # Adding model 'Contact'
+        db.create_table(u'companies_contact', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('position', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('phone', self.gf('django.db.models.fields.CharField')(max_length=20, blank=True)),
+            ('notes', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'companies', ['Contact'])
+
+        # Adding M2M table for field company on 'Contact'
+        m2m_table_name = db.shorten_name(u'companies_contact_company')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('contact', models.ForeignKey(orm[u'companies.contact'], null=False)),
+            ('company', models.ForeignKey(orm[u'companies.company'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['contact_id', 'company_id'])
+
+        # Adding model 'ApplyStatus'
+        db.create_table(u'companies_applystatus', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('status', self.gf('django.db.models.fields.CharField')(max_length=50)),
+        ))
+        db.send_create_signal(u'companies', ['ApplyStatus'])
+
+        # Adding model 'Position'
+        db.create_table(u'companies_position', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('company', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['companies.Company'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('location', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('start_date', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('date_noted', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
+            ('apply_portal', self.gf('django.db.models.fields.CharField')(max_length=500, blank=True)),
+            ('apply_deadline', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('apply_status', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['companies.ApplyStatus'], null=True, blank=True)),
+            ('notes', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'companies', ['Position'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Company'
+        db.delete_table(u'companies_company')
+
+        # Deleting model 'Contact'
+        db.delete_table(u'companies_contact')
+
+        # Removing M2M table for field company on 'Contact'
+        db.delete_table(db.shorten_name(u'companies_contact_company'))
+
+        # Deleting model 'ApplyStatus'
+        db.delete_table(u'companies_applystatus')
+
+        # Deleting model 'Position'
+        db.delete_table(u'companies_position')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'companies.applystatus': {
+            'Meta': {'object_name': 'ApplyStatus'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'companies.company': {
+            'Meta': {'object_name': 'Company'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        },
+        u'companies.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'company': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['companies.Company']", 'null': 'True', 'symmetrical': 'False'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'position': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
+        },
+        u'companies.position': {
+            'Meta': {'object_name': 'Position'},
+            'apply_deadline': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'apply_portal': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'apply_status': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['companies.ApplyStatus']", 'null': 'True', 'blank': 'True'}),
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['companies.Company']"}),
+            'date_noted': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['companies']
