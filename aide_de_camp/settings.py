7553b16b7272b5db461221b2f57624155802b29e
William Weiskopf <worldwidewilly@comcast.net>
2013-12-03 18:40:08 -0700

    Couple other setup details.

--- aide_de_camp/settings.py
+++ aide_de_camp/settings.py
@@ -172,6 +172,7 @@ import dj_database_url
 import os
 if os.getcwd() == "/app":
     DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    DEFAULT_FROM_EMAIL = 'app19174334@heroku.com'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
